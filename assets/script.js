84f8af9bbf5ba562c77b09e74cd8cf6e7bd01567
unknown <tyler.r.roll@gmail.com>
2021-08-14 13:13:47 -0600

    button fixes index

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,32 @@
-var classBtn = document.getElementsByClassName("button");
-var startClass = document.getElementsByClassName("columns")
-var nextPage = document.getElementById("continue")
-// var confirm = document.getElementsByClassName("hide")
+var classBtn = document.getElementsByClassName('button');
+var startClass = document.getElementsByClassName('columns')
+var nextPage = document.getElementById('continue')
+var y = document.getElementById('yes')
+var n = document.getElementById('no')
+// var confirm = document.getElementsByClassName('hide')
 
 
 function hideClass (){
     for (let i = 0; i < 3; i++) {
-    startClass[i].classList.add("hide")
+    startClass[i].classList.add('hide')
     }
-    // confirm.classList.remove("hide")
-    var ready = document.createElement("a")
-    ready.classList.add("button", "is-link", "is-large")
-    ready.href = "playerpage.html"
-    ready.textContent = "Ready"
-    nextPage.append(ready)
+    // confirm.classList.remove('hide')
+    var sure = document.createElement('h2')
+    sure.textContent = 'Are you sure this is the one you want?'
+    sure.classList.add('sticky')
+    var yes = document.createElement('a')
+    yes.classList.add('button', 'is-success', 'is-large', 'sticky1')
+    yes.innerText = 'Yes!'
+    yes.href = 'playerpage.html'
+    var no = document.createElement('a')
+    no.classList.add('button', 'is-danger', 'is-large', 'sticky2')
+    no.innerText = 'Wait, I\'m not sure';
+    no.href = 'index.html';
+    
+    nextPage.append(sure)
+    y.append(yes)
+    n.append(no)
+    
 }
 
 
